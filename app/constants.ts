5eeffd6e3aeff022ae4b0297a17100348fdf6979
Justin Steffen <jcsteffen03@gmail.com>
2026-03-06 10:21:51 -0600

    New Header updates

--- app/constants.ts
+++ app/constants.ts
@@ -5,6 +5,6 @@ export const COURSE_MAP_URL = "#course";
 export const HIGHLIGHT_REEL_URL =
   "https://next-wave-agentic.nyc3.cdn.digitaloceanspaces.com/ChasinPaytonMedia/2024ChasinPayson.mp4";
 export const HERO_VIDEO_URL =
-  "https://next-wave-agentic.nyc3.cdn.digitaloceanspaces.com/ChasinPaytonMedia/ChasinPaysonHero.mp4";
+  "https://next-wave-agentic.nyc3.cdn.digitaloceanspaces.com/ChasinPaytonMedia/ChasinPaysonHero2.mp4";
 export const EVENT_DATE_LABEL = "Thu, May 14 2026 • 5:00 PM";
 export const EVENT_VENUE = "Airship @ The Pumphouse, Bentonville, Arkansas";
