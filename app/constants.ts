8ee852e9dd03d0b88c7398b1d55477444f2379a2
Justin Steffen <jcsteffen03@gmail.com>
2026-03-06 13:06:25 -0600

    new url

--- app/constants.ts
+++ app/constants.ts
@@ -1,5 +1,4 @@
-export const KIKITS_REGISTER_URL =
-  "https://kikits.com/organizations/69a72fb647d530fa9c13f83f/events/69a88fd9191cc3d23d1c0a5a";
+export const KIKITS_REGISTER_URL = "https://kikits.com/api/qrcodes/aFQHNmM6";
 export const RULE_OF_THREE_URL = "https://ruleofthree.bike";
 export const COURSE_MAP_URL = "#course";
 export const HIGHLIGHT_REEL_URL =
